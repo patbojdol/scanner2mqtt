dc3886c3b222fe013b3061bc8781f17ce75764f3
Patrycja Bojdoł <pat.bojdol@gmail.com>
2022-10-25 22:17:01 +0200

    read env variable instead of passing a param

--- Program.cs
+++ Program.cs
@@ -5,41 +5,49 @@
 namespace CodeScanner
 {
 
-    public class Program
-    {
-        private static void Main(string[] args)
-        {
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			var port = Environment.GetEnvironmentVariable("SERIAL_PORT");
 
-            var scanner = new ScannerReader(args[0], 115200);
-            Console.ReadLine();
-        }
+			if (String.IsNullOrEmpty(port))
+			{
+				Console.WriteLine("You need to specify SERIAL_PORT environment variable");
+				System.Environment.Exit(1);
+			}
+			else
+			{
+				var scanner = new ScannerReader(port, 115200);
+				Console.ReadLine();
+			}
+		}
+	}
 
-    }
+	public class ScannerReader : IDisposable
+	{
+		private SerialPort _port;
 
-    public class ScannerReader : IDisposable
-    {
-        private SerialPort _port;
+		public ScannerReader(string portName, int baudRate)
+		{
+			_port = new SerialPort(portName, baudRate);
+			_port.Open();
+			_port.DataReceived += DataReceived;
+			Console.WriteLine("Ready.");
+		}
 
-        public ScannerReader(string portName, int baudRate)
-        {
-            _port = new SerialPort(portName, baudRate);
-            _port.Open();
-            _port.DataReceived += DataReceived;
-            Console.WriteLine("Ready.");
-        }
+		/// Serial event handler
+		void DataReceived(object s, SerialDataReceivedEventArgs e)
+		{
+			Console.WriteLine(_port.ReadLine());
+		}
 
-        /// Serial event handler
-        void DataReceived(object s, SerialDataReceivedEventArgs e)
-        {
-            Console.WriteLine(_port.ReadLine());
-        }
-
-        public void Dispose()
-        {
-            if (_port != null)
-            {
-                _port.Dispose();
-            }
-        }
-    }
+		public void Dispose()
+		{
+			if (_port != null)
+			{
+				_port.Dispose();
+			}
+		}
+	}
 }
