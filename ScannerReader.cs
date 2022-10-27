be33733e82283765ea3cb541dbb38e47303d2f89
Patrycja Bojdoł <patrycja@mrpb.eu>
2022-10-27 21:22:53 +0200

    add ean13 validation (#4)

--- ScannerReader.cs
+++ ScannerReader.cs
@@ -1,6 +1,7 @@
 namespace CodeScanner
 {
 	using System.IO.Ports;
+	using System.Text.RegularExpressions;
 
 	public class ScannerReader : IDisposable
 	{
@@ -14,10 +15,42 @@ namespace CodeScanner
 			Console.WriteLine("Ready.");
 		}
 
+		bool ValidateEAN13(string code)
+		{
+			if (!Regex.IsMatch(code, @"^\d{13}$"))
+			{
+				return false;
+			}
+
+			var codeNumbers = code.Select(digit => int.Parse(digit.ToString())).ToArray();
+
+			int even = 0, odd = 0;
+			for (var i = 0; i < code.Length - 1; i += 1)
+			{
+				if (i % 2 == 0) even += codeNumbers[i];
+				else odd += codeNumbers[i];
+			}
+
+			int total = even + odd * 3;
+			int checksum = total % 10;
+			if (checksum != 0) checksum = 10 - checksum;
+
+			return codeNumbers.Last() == checksum;
+		}
+
 		/// Serial event handler
 		void DataReceived(object s, SerialDataReceivedEventArgs e)
 		{
-			Console.WriteLine(_port.ReadLine());
+			var code = _port.ReadLine().Trim();
+
+			if (ValidateEAN13(code))
+			{
+				Console.WriteLine($"EAN13: {code} valid");
+			}
+			else
+			{
+				Console.WriteLine(code);
+			}
 		}
 
 		public void Dispose()
